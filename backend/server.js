5884f78a53c4a7ab7e0108ee0fab45d4bdfb1dd6
Shivam Chamoli <shivamchamoli03@gmail.com>
2025-12-10 06:26:26 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -46,12 +46,13 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/favorites", favoriteRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
 	});
 }
+
 
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
